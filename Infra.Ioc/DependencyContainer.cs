e5014c51802591ce5102fb6cb9aa188c8f5c8eec
Elham Souri <ElhamSouri@outlook.com>
2024-07-28 14:29:20 +0330

    impeliment Ioc

---
+++ Infra.Ioc/DependencyContainer.cs
@@ -0,0 +1,19 @@
+﻿using Application.Interfaces;
+using Application.Services;
+using Domain.Interfaces;
+using Infra.Data.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infra.Ioc
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection service) {
+            //Application Layer
+            service.AddScoped<IPocService, PocServices>();
+
+            //Infra.Data Layer
+            service.AddScoped<IPocRepository, PocRepository>();
+        }
+    }
+}
