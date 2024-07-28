a2d2471d890585d586491ce5db582373c560155e
Elham Souri <ElhamSouri@outlook.com>
2024-07-28 13:08:23 +0330

    create projects and Poc class

---
+++ Poc.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Poc
+{
+    public Guid Id { get; set }
+    public string Username { get; set }
+    public string Password { get; set }
+    public bool IsActive { get; set }
+    public DateTime ExpireDate { get; set }
+
+}
